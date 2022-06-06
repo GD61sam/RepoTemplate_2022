37852ed1e3513bdbc12647728c7746ba12abf984
Sam McKinney <iamsammckinney@gmail.com>
2022-06-06 16:02:48 -0700

    added project and basic set up

---
+++ Assets/Scripts/UI/SetCursorLockMode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCursorLockMode : MonoBehaviour
+{
+    private void SetCursorVisable(bool isVisable)
+    {
+        Cursor.visible = isVisable;
+    }
+
+    public void SetModeNone()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SetCursorVisable(true);
+    }
+
+    public void SetModeConfined()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        SetCursorVisable(true);
+    }
+
+    public void SetModeLocked()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        SetCursorVisable(false);
+    }
+}
